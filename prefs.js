61dfe5a260e049e379c8e38a34afb5b39415a37e
miro011 <miro9961@gmail.com>
2025-01-09 15:37:59 -0700

    prefs.js standardization

--- prefs.js
+++ prefs.js
@@ -7,100 +7,90 @@ import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/
 export default class Prefs extends ExtensionPreferences {
 
     fillPreferencesWindow(window) {
-        let settings = this.getSettings();
+        this.settings = this.getSettings();
 
-        // Create Preferences Page
-        const preferencesPage = new Adw.PreferencesPage();
+        // Create Preferences Page and group
+        let preferencesPage = new Adw.PreferencesPage();
+        let preferencesGroup = new Adw.PreferencesGroup({title: _('Workspace Buttons Settings'),});
 
-        // Create Preferences Group
-        const preferencesGroup = new Adw.PreferencesGroup({
-            title: _('Workspace Buttons Settings'),
-        });
+        // Generate and append rows
+        let rowsArr = [
+            this.get_int_spin_row('app-icon-size', 'Icon Size', 'Set the size of the app icons (10px to 96px)', 10, 96),
+            this.get_int_spin_row('workspace-number-font-size', 'Workspace Number Font Size', 'Set the font size of the workspace numbers (10px to 96px)', 10, 96),
+            this.get_color_picker_row('active-workspace-color', 'Active Workspace Color', 'Set the color of the active workspace')
+        ];
+        for (let row of rowsArr) {
+            preferencesGroup.add(row);
+        }
 
-        // Icon Size Setting
-        const iconSizeRow = new Adw.ActionRow({
-            title: _('Icon Size'),
-            subtitle: _('Set the size of the app icons (10px to 96px)'),
-        });
-
-        const iconSizeSpinButton = new Gtk.SpinButton({
-            adjustment: new Gtk.Adjustment({
-                lower: 10,
-                upper: 96,
-                step_increment: 1,
-            }),
-            value: settings.get_int('app-icon-size'),
-        });
-
-        iconSizeSpinButton.connect('value-changed', w => {
-            settings.set_int('app-icon-size', w.get_value_as_int());
-        });
-
-        iconSizeRow.add_suffix(iconSizeSpinButton);
-        iconSizeRow.activatable_widget = iconSizeSpinButton;
-
-        // Workspace Number Font Size Setting
-        const fontSizeRow = new Adw.ActionRow({
-            title: _('Workspace Number Font Size'),
-            subtitle: _('Set the font size of the workspace numbers (10px to 96px)'),
-        });
-
-        const fontSizeSpinButton = new Gtk.SpinButton({
-            adjustment: new Gtk.Adjustment({
-                lower: 10,
-                upper: 96,
-                step_increment: 1,
-            }),
-            value: settings.get_int('workspace-number-font-size'),
-        });
-
-        fontSizeSpinButton.connect('value-changed', w => {
-            settings.set_int('workspace-number-font-size', w.get_value_as_int());
-        });
-
-        fontSizeRow.add_suffix(fontSizeSpinButton);
-        fontSizeRow.activatable_widget = fontSizeSpinButton;
-
-        // Active Workspace Color Setting
-        const colorRow = new Adw.ActionRow({
-            title: _('Active Workspace Color'),
-            subtitle: _('Set the color of the active workspace'),
-        });
-
-        const colorButton = new Gtk.ColorButton({
-            rgba: this._parseColor(settings.get_string('active-workspace-color')),
-        });
-
-        colorButton.connect('color-set', w => {
-            const rgba = w.get_rgba();
-            settings.set_string('active-workspace-color', this._colorToHex(rgba));
-        });
-
-        colorRow.add_suffix(colorButton);
-        colorRow.activatable_widget = colorButton;
-
-        // Add Rows to Group and Group to Page
-        preferencesGroup.add(iconSizeRow);
-        preferencesGroup.add(fontSizeRow);
-        preferencesGroup.add(colorRow);
+        // Add Group to Page and Page to the Window
         preferencesPage.add(preferencesGroup);
-
-        // Add Page to the Window
         window.add(preferencesPage);
 
-        // Set Window Default Size
         window.set_default_size(750, 580);
+        // comment above and uncomment below to make window adaptable
+        // window.set_resizable(true);
+    }
+
+    get_int_spin_row(dconfKey, title, subtitle, min, max, increment=1) {
+        let row = new Adw.ActionRow({
+            title: _(title),
+            subtitle: _(subtitle),
+        });
+
+        let spinBtn = new Gtk.SpinButton({
+            adjustment: new Gtk.Adjustment({
+                lower: min,
+                upper: max,
+                step_increment: increment,
+            }),
+            value: this.settings.get_int(dconfKey),
+        });
+
+        // spinBtn.set_tooltip_text(_('blah.'));
+
+        spinBtn.connect('value-changed', sb => {
+            this.settings.set_int(dconfKey, sb.get_value_as_int());
+        });
+
+        row.add_suffix(spinBtn);
+        row.activatable_widget = spinBtn;
+
+        return row;
+    }
+
+    get_color_picker_row(dconfKey, title, subtitle) {
+        let row = new Adw.ActionRow({
+            title: _(title),
+            subtitle: _(subtitle),
+        });
+
+        let colorButton = new Gtk.ColorButton({
+            rgba: this.get_rgba_color_from_hex(this.settings.get_string(dconfKey)),
+        });
+
+        // colorButton.set_tooltip_text(_('blah.'));
+
+        colorButton.connect('color-set', cb => {
+            const rgba = cb.get_rgba();
+            this.settings.set_string(dconfKey, this.get_hex_color_from_rgba(rgba));
+        });
+
+        row.add_suffix(colorButton);
+        row.activatable_widget = colorButton;
+
+        return row;
     }
 
     // Helper: Parse a hex color string to a Gdk.RGBA object
-    _parseColor(hex) {
+    get_rgba_color_from_hex(hex) {
         const rgba = new Gdk.RGBA();
         rgba.parse(hex);
         return rgba;
     }
 
     // Helper: Convert a Gdk.RGBA object to a hex string
-    _colorToHex(rgba) {
+    get_hex_color_from_rgba(rgba) {
         const r = Math.round(rgba.red * 255).toString(16).padStart(2, '0');
         const g = Math.round(rgba.green * 255).toString(16).padStart(2, '0');
         const b = Math.round(rgba.blue * 255).toString(16).padStart(2, '0');
